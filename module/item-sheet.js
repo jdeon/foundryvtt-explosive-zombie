dccda111e73ca4c1185b9fbe73c41a681a355aea
jdeon <jd.eymann@gmail.com>
2026-09-02 01:00:10 +0200

    feat: add update on item-sheet

--- module/item-sheet.js
+++ module/item-sheet.js
@@ -2,7 +2,7 @@ import { EntitySheetHelper } from "./helper.js";
 import { ATTRIBUTE_TYPES } from "./constants.js";
 
 /**
- * Extend the basic ItemSheet with some very simple modifications
+ * Extend the basic ItemSheet with custom tabbed sheet layout and effect management
  * @extends {ItemSheet}
  */
 export class SimpleItemSheet extends ItemSheet {
@@ -12,10 +12,10 @@ export class SimpleItemSheet extends ItemSheet {
     return foundry.utils.mergeObject(super.defaultOptions, {
       classes: ["worldbuilding", "sheet", "item"],
       template: "systems/explosive-zombie/templates/item-sheet.html",
-      width: 520,
-      height: 480,
-      tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description" }],
-      scrollY: [".attributes"],
+      width: 580,
+      height: 620,
+      tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "sheet" }],
+      scrollY: [".card-container", ".classic-form", ".attributes"],
     });
   }
 
@@ -27,7 +27,14 @@ export class SimpleItemSheet extends ItemSheet {
     EntitySheetHelper.getAttributeData(context.data);
     context.systemData = context.data.system;
     context.dtypes = ATTRIBUTE_TYPES;
-    context.descriptionHTML = await TextEditor.enrichHTML(context.systemData.description, {
+
+    // Ensure array defaults exist safely
+    if (!context.systemData.activeEffects) context.systemData.activeEffects = [];
+    if (!context.systemData.passiveEffects) context.systemData.passiveEffects = [];
+
+    context.imageUrl = context.systemData.imageUrl || context.data.img || "icons/svg/item-bag.svg";
+
+    context.descriptionHTML = await TextEditor.enrichHTML(context.systemData.description || "", {
       secrets: this.document.isOwner,
       async: true
     });
@@ -43,6 +50,9 @@ export class SimpleItemSheet extends ItemSheet {
     // Everything below here is only needed if the sheet is editable
     if (!this.isEditable) return;
 
+    // Effect management in Classic Form
+    html.find('.effect-control').click(this._onEffectControl.bind(this));
+
     // Attribute Management
     html.find(".attributes").on("click", ".attribute-control", EntitySheetHelper.onClickAttributeControl.bind(this));
     html.find(".groups").on("click", ".group-control", EntitySheetHelper.onClickAttributeGroupControl.bind(this));
@@ -56,6 +66,31 @@ export class SimpleItemSheet extends ItemSheet {
         ev.dataTransfer.setData('text/plain', JSON.stringify(dragData));
       }, false);
     });
+  }
+
+  /**
+   * Handle active and passive effect creation and deletion
+   * @param {Event} event
+   * @private
+   */
+  async _onEffectControl(event) {
+    event.preventDefault();
+    const button = event.currentTarget;
+    const action = button.dataset.action;
+    const type = button.dataset.type || "activeEffects";
+    const effects = Array.from(this.item.system[type] || []);
+
+    if (action === "add") {
+      if (type === "activeEffects") {
+        effects.push({ actionNumber: "", threshold: "", dice: "", ammo: "", description: "" });
+      } else {
+        effects.push({ description: "" });
+      }
+    } else if (action === "delete") {
+      const index = Number(button.dataset.index);
+      if (!isNaN(index)) effects.splice(index, 1);
+    }
+    return this.item.update({ [`system.${type}`]: effects });
   }
 
   /* -------------------------------------------- */
